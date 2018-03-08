a6881fde97373af55b4940df5e2a66a7e13d229a
saitilakv <saitilak.vakkalagadda@campsystems.com>
2018-03-08 17:03:15 +0530

    Second commit

---
+++ 22. Program to find sum of first 10 natural numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Program_to_find_sum_of_first_10_natural_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int sum=0;
+
+            Console.Write("The first 10 natural numbers are:");
+
+            for(int i=0;i<number1.Length;i++)
+            {
+                Console.Write(number1[i]);
+                sum = sum + number1[i];
+
+            }
+            Console.WriteLine('\n');
+            Console.WriteLine("Sum of first ten natural numbers is: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SessionPractice/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SessionPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] number1= { 37, 75, 51, 65, 99, 80 };
+            int pass=0, fail=0;
+            //Console.WriteLine("Enter a number");
+            //number1 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < number1.Length; i++)
+            {
+                //if ((number1[i] > 1) && (number1[i] <= 100))
+                //{
+                if (number1[i] >= 35)
+                {
+                    //if (number1[i] >= 60)
+                    //{
+                    //    Console.WriteLine("You have passed subject" + i + " with first class.");
+                    //}
+                    //else if (number1[i] >= 50 && number1[i] < 60)
+                    //{
+                    //    Console.WriteLine("You have passed subject" + i + " with second class.");
+                    //}
+                    //else
+                    //{
+                    //    Console.WriteLine("You have passed subject" + i + " with third class.");
+                    //}
+                    pass++;
+
+                }
+                else
+                {
+                    fail++;
+                    break;
+                }
+
+            }
+            if(fail>0)
+            {
+                Console.WriteLine("Better Luck Next Time");
+            }
+            else
+            {
+                Console.WriteLine("You have successfully cleared all subjects");
+            }
+              
+                        Console.ReadLine();
+        }
+    }
+}
