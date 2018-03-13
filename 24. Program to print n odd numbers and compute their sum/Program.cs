c8a7f80076bcc7d2707c26cfc258840000c55366
saitilakv <saitilak.vakkalagadda@campsystems.com>
2018-03-13 15:27:49 +0530

    Programs for right angled triangel and print n odd terms

---
+++ 24. Program to print n odd numbers and compute their sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Program_to_print_n_odd_numbers_and_compute_their_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number,sum=0;
+
+            Console.WriteLine("Enter number of terms");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            var number1 = new int[number];
+            var count = 0;
+            for (int i = 1; i > 0; i++)
+            {
+                if (count == number)
+                {
+                    break;
+                }
+                else if ((i % 2) != 0)
+                {
+                    number1[count] = i;
+                    sum = sum + number1[count];
+                    count++;
+                }
+               
+            }
+            Console.WriteLine(String.Format("Odd numbers array is: [ {0} ]", String.Join(",", number1)));
+            Console.WriteLine("Sum of first " + number + " odd numbers is:" + sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 25. Program to print a right angled triangle in a given format/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25.Program_to_print_a_right_angled_triangle_in_a_given_format
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number,height;
+            var strConcat="1";
+            Console.WriteLine("Enter value for height:");
+            height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(strConcat);
+            for (number=2;number<=height;number++)
+            {
+                strConcat = string.Format("{0}{1}",strConcat,number);
+                Console.WriteLine(strConcat);
+                  
+            }
+            Console.Read();
+
+            
+        }
+    }
+}
