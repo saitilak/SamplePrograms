00a411a6f65b96a474b76c357944d8d0d6f70566
saitilakv <saitilak.vakkalagadda@campsystems.com>
2018-03-23 15:52:05 +0530

    To print sum of 1/n terms

--- 30. Program to print sum of n terms of a series/Program.cs
+++ 30. Program to print sum of n terms of a series/Program.cs
@@ -10,6 +10,24 @@ namespace _30.Program_to_print_sum_of_n_terms_of_a_series
     {
         static void Main(string[] args)
         {
+            int numberOfTerms;
+            double sum = 0.000;
+            Console.WriteLine("Enter number of terms:");
+            numberOfTerms = Convert.ToInt32(Console.ReadLine());
+            if(numberOfTerms>0)
+            { 
+            for (int i=1;i<=numberOfTerms;i++)
+                {
+                    double temporary = Convert.ToDouble(1 / i);
+                    sum = sum + temporary;
+                }
+                Console.WriteLine("Sum onf entered terms is: " + sum);
+            }
+            else
+            {
+                Console.WriteLine("Enter a positive integer greater than 0");
+            }
+            Console.ReadLine();
         }
     }
 }
