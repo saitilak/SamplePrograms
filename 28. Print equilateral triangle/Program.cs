1793b9896e5725059715319d97efdcfdc6917afd
saitilakv <saitilak.vakkalagadda@campsystems.com>
2018-03-23 14:18:31 +0530

    Printing equilateral triangle

---
+++ 28. Print equilateral triangle/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28.Print_equilateral_triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Height;
+            int width=1;
+            int number = 1;
+            int spaceDecrement;
+            
+            Console.WriteLine("Enter the height:");
+            Height = Convert.ToInt32(Console.ReadLine());
+            spaceDecrement = Height;
+            for (int i = 1; i <= Height; i++)
+            {
+                for(int space= spaceDecrement - 1;space>0;space--)
+                {
+                    Console.Write(" ");
+                    
+                }
+                spaceDecrement--;
+
+                for (int j= 1;j<= width;j++ )
+                {
+                    Console.Write(number);
+                    Console.Write(" ");
+                    number++;
+                }
+                width++;
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+           
+
+        }
+    }
+}
