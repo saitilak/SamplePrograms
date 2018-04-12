75dfd546f5ce9a02f7c38c5e3eb0b281c9aee9d2
saitilakv <saitilak.vakkalagadda@campsystems.com>
2018-04-12 13:44:54 +0530

    New programs

---
+++ 32.Program to find if a number is perfect number/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32.Program_to_find_if_a_number_is_perfect_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber;
+            int sum = 0;
+            Console.WriteLine("Enter the number to verify if it is a Perfect Number");
+            inputNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Positive divisors are:");
+            for (int i=1;i<inputNumber;i++)
+            {
+                if((inputNumber%i)==0)
+                {
+                    sum = sum + i;
+                    
+                    Console.Write(i);
+                }
+            }
+            Console.WriteLine("Sum of divisors:" + sum);
+            if(sum==inputNumber)
+            {
+                Console.WriteLine("Given number is a Perfect number");
+            }
+            else
+            {
+                Console.WriteLine("Given number is not a perfect number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Session Practice1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            simpleCalculator a = new complexCalculator();
+            int total=a.sum(4, 5);
+            Console.WriteLine(total);
+            Console.Read();
+        }
+    }
+}
